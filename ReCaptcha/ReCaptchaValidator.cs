331124f56ee210a3eeba2f1c15e9719a2b04ebcf
Ion Ursachi <nunutu29@users.noreply.github.com>
2022-06-17 10:55:13 +0200

    Update ReCaptchaValidator.cs

--- ReCaptcha/ReCaptchaValidator.cs
+++ ReCaptcha/ReCaptchaValidator.cs
@@ -15,18 +15,25 @@ public class ReCaptchaValidator
 
     public async Task<bool> ValidateAsync(string reCaptcha)
     {
+        // ask for HttpClient
         var client = _httpFactory.CreateClient();
 
+        // create the request uri and assign secret and response parameters
         var uriBuilder = new UriBuilder(_options.RequestUri)
         {
             Query = $"secret={_options.Secret}&response={reCaptcha}"
         };
 
+        // create the request message with POST method
+        // https://developers.google.com/recaptcha/docs/verify
         using var message = new HttpRequestMessage(HttpMethod.Post, uriBuilder.Uri);
+        
+        // send the request
         using var response = await client.SendAsync(message);
 
         if (response.IsSuccessStatusCode)
         {
+            // read the response and check if succeed
             var output = await response.Content.ReadFromJsonAsync<RequestOutput>();
 
             if (output != null)
